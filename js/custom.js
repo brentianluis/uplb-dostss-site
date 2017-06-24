bd24bbc118aa62a82aec03b648dc2d1c1f867f22
Brent Ian Luis <brentianluis@gmail.com>
2017-06-25 01:25:08 +0800

    Update HTML, CSS, JS files

--- js/custom.js
+++ js/custom.js
@@ -1,16 +1,24 @@
+/**
+ *	UPLB DOST Scholars' Society Website v1.0 (https://github.com/brentianluis/uplb-dostss-site)
+ *	MIT License (https://github.com/brentianluis/uplb-dostss-site/blob/master/LICENSE)
+ *	Copyright (c) 2017 Brent Ian Luis
+ */
 
 // Google Map
 function orgMap() {
-	var apt = {lat: 14.168191, lng: 121.245037};
+    var mapOptions = {
+        zoom: 16,
+        center: new google.maps.LatLng(14.168191, 121.245037), //org house
+        //styles: [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"on"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#46bcec"},{"visibility":"on"}]}]
+    };
 
-	var map = new google.maps.Map(document.getElementById("map"),{
-        center: apt,
-        zoom: 17,
-        mapTypeId: google.maps.MapTypeId.HYBRID
-	});
+    var mapElement = document.getElementById('map');
+    var map = new google.maps.Map(mapElement, mapOptions);
 
+    // Marker
     var marker = new google.maps.Marker({
-      position: apt,
-      map: map
+        position: new google.maps.LatLng(14.168191, 121.245037),
+        map: map,
+        title: 'UPLB DOST SS Organization House'
     });
-}
+}//orgMap
